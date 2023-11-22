189c0500dea481ee4fcbef849eb57a57e88f8def
wagner.rodrigues <wagneroliveirarodrigues@gmail.com>
2023-11-22 14:17:48 -0300

    feat: create entity transaction

---
+++ picpay/src/main/java/com/picpay/domain/transaction/Transaction.java
@@ -0,0 +1,40 @@
+package com.picpay.domain.transaction;
+
+import com.picpay.domain.user.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Data;
+
+@Data
+@Entity(name="transactions")
+@Table(name="transactions")
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "payer_id")
+    private User payer;
+
+    @ManyToOne
+    @JoinColumn(name = "payee_id")
+    private User payee;
+
+    private LocalDateTime timestamp;
+}
